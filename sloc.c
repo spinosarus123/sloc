73a6ef24e787bf48ae60478df0710642d26f7edb
Isak Ellmer <isak01@gmail.com>
2022-12-04 15:23:07 +0100

    Update sloc.c

--- sloc.c
+++ sloc.c
@@ -1,4 +1,4 @@
-/* sloc, a simple source line counter.
+/* sloc, a simple source lines of code counter.
  * Copyright (C) 2022 Isak Ellmer
  *
  * This program is free software: you can redistribute it and/or modify
@@ -15,12 +15,16 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 enum {
 	physical, logical, comment, end
+};
+
+struct counter {
+	unsigned long physical;
+	unsigned long logical;
+	unsigned long comment;
 };
 
 struct flag {
@@ -32,158 +36,135 @@ struct flag {
 	
 	int physical;
 	int logical;
+	int comment;
 	
-	int parentheses;
+	int paren;
+	int curly;
 
 	int escapechar;
+
+	char lastchar;
 };
 
-int lookahead(const char *str, const char c) {
-	for (int i = 0; str[i]; i++) {
-		if (str[i] == c)
-			return 1;
-		if (str[i] != ' ' && str[i] != '\t')
-			return 0;
+int parsechar(const char c, struct counter *counter, struct flag *flag) {
+	if (flag->escapechar) {
+		flag->escapechar = 0;
 	}
-	return 0;
-}
-
-void loc(const char *str, unsigned long count[end], struct flag *flag) {
-	//printf("%i, %i, %i, %i - %s", flag->mlc, flag->slc, flag->str, flag->chr, str);
-	//printf("%s", str);
-	for (char *ptr = (char *)str; *ptr; ptr++) {
-		if (!flag->mlc && !flag->slc && !flag->str && !flag->chr &&
-				*ptr == '#') {
-			flag->pre = 1;
-		}
-		else if (!flag->mlc && !flag->slc && !flag->str && !flag->chr &&
-				strncmp(ptr, "/*", 2) == 0) {
-			flag->mlc = 1;
-		}
-		else if (flag->mlc && ptr >= str + 2 &&
-				strncmp(ptr - 2, "*/", 2) == 0) {
+	else if (flag->mlc) {
+		if (c == '\n')
+			counter->comment++;
+		if (flag->lastchar == '*' && c == '/') {
 			flag->mlc = 0;
-			count[comment]++;
-			//printf("%s", str);
+			counter->comment++;
 		}
-		else if (!flag->mlc && !flag->slc && !flag->chr &&
-				*ptr == '"' && flag->escapechar % 2 == 0) {
-			flag->str ^= 1;
-		}
-		else if (!flag->mlc && !flag->slc && !flag->str &&
-				*ptr == '\'' && flag->escapechar % 2 == 0) {
-			flag->chr ^= 1;
-		}
-		else if (!flag->mlc && !flag->str && !flag->chr &&
-				strncmp(ptr, "//", 2) == 0) {
-			flag->slc = 1;
-		}
-
-		switch (*ptr) {
-		case '\n':
-			/* line continuation */
-			if (flag->escapechar % 2 == 1) {
-				if (flag->physical) {
-					//printf("%s", str);
-					count[physical]++;
-				}
-				break;
-			}
-
-			if (flag->mlc) {
-				count[comment]++;
-				//printf("%s", str);
-			}
-			else if (flag->slc) {
-				count[comment]++;
-				//printf("%s", str);
-			}
-
-			if (flag->pre) {
-				printf("%s", str);
-				count[logical]++;
-			}
-			if (flag->physical) {
-				//printf("%s", str);
-				count[physical]++;
-			}
-
-			flag->pre = 0;
-			flag->slc = 0;
-
-			flag->physical = 0;
-			flag->logical = 0;
-
-			break;
-		case ';':
-			if (!flag->pre && !flag->mlc && !flag->slc && !flag->str &&
-					!flag->chr && flag->parentheses == 0) {
-				printf("%s", str);
-				count[logical]++;
-				flag->logical = 0;
-			}
-		case ' ':
-		case '\t':
-			break;
-		case '(':
-			if (!flag->pre && !flag->mlc && !flag->slc && !flag->str &&
-					!flag->chr)
-				flag->parentheses++;
-			/* fallthrough */
-		case ')':
-			if (!flag->pre && !flag->mlc && !flag->slc && !flag->str &&
-					!flag->chr) {
-				flag->physical = 1;
-				if (*ptr == ')') {
-					flag->parentheses--;
-					if (flag->logical && flag->parentheses == 0 &&
-							!lookahead(ptr + 1, ';')) {
-						printf("%s", str);
-						count[logical]++;
-						flag->logical = 0;
-						flag->parentheses = 0;
-					}
-					break;
-				}
-			}
-			/* fallthrough */
-		default:
-			if (!flag->mlc && !flag->slc && !flag->str && !flag->chr)
-				flag->logical = 1;
-			/* fallthrough */
-		case '{':
-		case '}':
-			if (!flag->mlc && !flag->slc)
-				flag->physical = 1;
-			break;
-		}
-
-		if (*ptr == '\\')
-			flag->escapechar++;
-		else
-			flag->escapechar = 0;
 	}
+	else if (flag->slc) {
+		if (c == '\n') {
+			counter->comment++;
+			flag->slc = 0;
+		}
+	}
+	else if (flag->str) {
+		if (c == '"')
+			flag->str = 0;
+	}
+	else if (flag->chr) {
+		if (c == '\'')
+			flag->chr = 0;
+	}
+	else switch (c) {
+	case EOF:
+		return 0;
+	case '\n':
+		if (flag->physical)
+			counter->physical += 1;
+
+		if (flag->pre) {
+			counter->logical++;
+		}
+		else {
+			counter->logical += flag->logical;
+		}
+
+		flag->pre = 0;
+		flag->curly = 0;
+
+		flag->physical = 0;
+		flag->logical = 0;
+		break;
+	case '\\':
+		flag->escapechar = 1;
+		break;
+	case '#':
+		flag->pre = 1;
+		break;
+	case '*':
+		if (flag->lastchar == '/')
+			flag->mlc = 1;
+		break;
+	case '/':
+		if (flag->lastchar == '/')
+			flag->slc = 1;
+		break;
+	case '\'':
+		flag->chr = 1;
+		break;
+	case ' ':
+	case '\t':
+		break;
+	case '"':
+		flag->physical = 1;
+		flag->str = 1;
+		break;
+	case ';':
+		flag->physical = 1;
+		if (!flag->curly)
+			flag->logical++;
+		break;
+	case ':':
+		flag->physical = 1;
+		if (!flag->logical)
+			flag->logical = 1;
+		break;
+	case '(':
+		flag->physical = 1;
+		flag->paren++;
+		break;
+	case ')':
+		flag->physical = 1;
+		flag->paren--;
+		break;
+	case '{':
+		flag->logical++;
+		flag->physical = 1;
+		break;
+	case '}':
+		flag->curly = 1;
+		/* fallthrough */
+	default:
+		flag->physical = 1;
+		break;
+	}
+
+	printf("%c", c);
+	flag->lastchar = c;
+	return 1;
 }
 
-int sloc(FILE *file, unsigned long count[end]) {
+int sloc(FILE *file, struct counter *counter) {
 	if (!file)
 		return 1;
-	for (int i = 0; i < end; i++)
-		count[i] = 0;
 	struct flag flag = { 0 };
-	char buffer[128];
-	while (fgets(buffer, sizeof(buffer), file)) {
-		loc(buffer, count, &flag);
-	}
+	while (parsechar(fgetc(file), counter, &flag));
 	return 0;
 }
 
 int main(int argc, char **argv) {
 	FILE *file = fopen(argv[1], "r");
-	unsigned long count[end];
-	sloc(file, count);
-	printf("physical: %lu\n", count[physical]);
-	printf("logical: %lu\n", count[logical]);
-	printf("comment: %lu\n", count[comment]);
+	struct counter counter = { 0 };
+	sloc(file, &counter);
+	printf("physical: %lu\n", counter.physical);
+	printf("logical: %lu\n", counter.logical);
+	printf("comment: %lu\n", counter.comment);
 	return 0;
 }
